d0dbfc3bd6e5efae2477e09a629cc511f022e233
Theodor <fracter.giftogod@gmail.com>
2026-01-31 16:18:24 +0100

    Reorganize main script

--- src/main.gd
+++ src/main.gd
@@ -8,6 +8,10 @@ const PREL_PLAYER = preload("uid://ctac7w7mgcdq8")
 var player_nodes: Dictionary[int, Player] = {}
 
 
+#
+# ---- PROCEDURE ----
+#
+
 func _ready() -> void:
 	Lobby.connected.connect(_on_connected)										# NOTE Local connected only
 	Lobby.disconnected.connect(_on_disconnected)
@@ -19,8 +23,13 @@ func _ready() -> void:
 	multiplayer_spawner.despawned.connect(_check_if_player_despawned)
 
 
+#
+# ---- SIGNALS ----
+#
+
 func _on_connected() -> void:
 	multiplayer.multiplayer_peer = Lobby.multiplayer.multiplayer_peer
+
 
 func _on_disconnected(message: String) -> void:
 	# TODO Quit level? set multiplayer peer again?
@@ -39,17 +48,6 @@ func _on_peer_disconnected(peer_id: int) -> void:
 		player_nodes.erase(peer_id)												# NOTE player_nodes is kept synced on remote peers by the MultiplayerSpawner signal callbacks
 
 
-func _spawn_player(id: int) -> void:
-	assert(multiplayer.is_server())
-	assert(not player_nodes.has(id), "in _spawn_player() Spawning a player that is already registered!")
-
-	var player_instance: Player = PREL_PLAYER.instantiate()
-	player_instance.peer_id = id
-	player_nodes[id] = player_instance											# NOTE player_nodes is kept synced on remote peers by the MultiplayerSpawner signal callbacks
-	networked_entities.add_child(player_instance)
-
-#func _despawn_player(id: int) -> void:
-
 ## Adds the node to [member player_nodes] if it is a [Player]
 func _check_if_player_spawned(node: Node) -> void:
 	assert(not multiplayer.is_server(), "_on_entity_spawned() should only be called by non-servers, as described in the MultiplayerSpawner signal description.")
@@ -63,3 +61,17 @@ func _check_if_player_despawned(node: Node) -> void:
 	if node is Player:
 		assert(player_nodes.has(node.peer_id), "in _on_entity_despawned(), the deleted player should still be in the player_nodes dict!")
 		player_nodes.erase(node.peer_id)
+
+
+#
+# ---- INTERNALS ----
+#
+
+func _spawn_player(id: int) -> void:
+	assert(multiplayer.is_server())
+	assert(not player_nodes.has(id), "in _spawn_player() Spawning a player that is already registered!")
+
+	var player_instance: Player = PREL_PLAYER.instantiate()
+	player_instance.peer_id = id
+	player_nodes[id] = player_instance											# NOTE player_nodes is kept synced on remote peers by the MultiplayerSpawner signal callbacks
+	networked_entities.add_child(player_instance)
