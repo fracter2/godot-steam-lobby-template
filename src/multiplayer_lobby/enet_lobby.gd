dddb12f5f564544cc5415f04da70d0f480819299
Theodor <fracter.giftogod@gmail.com>
2026-02-07 02:38:24 +0100

    Complete enet lobby hosting and joining code

--- src/multiplayer_lobby/enet_lobby.gd
+++ src/multiplayer_lobby/enet_lobby.gd
@@ -6,6 +6,9 @@ var init_as_host:bool
 var init_ip: String
 var init_port: int
 
+var max_clients: int = 1000	# NOTE limited by ENetMultiplayerPeer.create_server
+
+
 func _init(is_host: bool = false, ip: String = "127.0.0.1", port: int = 8080, username_: String = "DefaultName") -> void:
 	init_as_host = is_host
 	username = username_
@@ -21,20 +24,34 @@ func is_lobby_owner() -> bool:
 
 
 func initiate_connection() -> bool:
-	var peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+	# TODO Validate port and ip
+
+	_create_multiplayer_peer()
 	if init_as_host:
-		return false
+		return _initiate_as_host()
 	else:
-		# TODO Validate port and ip
-		var error: Error = peer.create_client(init_ip, init_port)
-		if error != Error.OK:
-			push_warning("error while trying to EnetMultiplayerPeer.create_client(), error: " + str(error))
-			return false
-		else:
-			multiplayer_peer = peer
-			_delegate_join_lobby.call_deferred()
-			connected_as_client.emit()
-			return true
+		return _initiate_as_client()
+
+
+func _initiate_as_host() -> bool:
+	var error: Error = (multiplayer_peer as ENetMultiplayerPeer).create_server(init_port, max_clients)
+	if error != Error.OK:
+		push_warning("error while trying to EnetMultiplayerPeer.create_client(), error: " + str(error))
+		return false
+
+	connected_as_host.emit()
+	return true
+
+
+func _initiate_as_client() -> bool:
+	var error: Error = (multiplayer_peer as ENetMultiplayerPeer).create_client(init_ip, init_port)
+	if error != Error.OK:
+		push_warning("error while trying to EnetMultiplayerPeer.create_client(), error: " + str(error))
+		return false
+
+	#_delegate_join_lobby.call_deferred()									# TODO IS THIS NESSESSARY?
+	connected_as_client.emit()
+	return true
 
 func _delegate_join_lobby() -> void:
 	connected_as_client.emit()
@@ -44,3 +61,29 @@ func _delegate_host_lobby() -> void:
 
 func _delegate_on_disconnected(message: String) -> void:
 	disconnected.emit(message)
+
+
+func _create_multiplayer_peer() -> void:
+	var peer: ENetMultiplayerPeer = ENetMultiplayerPeer.new()
+	peer.peer_connected.connect(_on_peer_connected)
+	peer.peer_disconnected.connect(_on_peer_disconnected)
+	multiplayer_peer = peer
+	#multiplayer_peer_set.emit(peer)	# TODO CONSIDER REMOVING UNSUED
+
+func _on_peer_connected(peer_id: int) -> void:
+	if Lobby.players.has(peer_id):
+		print_debug("in SteamMultiplayerLobby._on_peer_connected(%d), somehow already have the peer_id in the players[] dict" % peer_id)
+	else:
+		Lobby.add_new_player_info(peer_id)
+
+	var steam_id: int = (multiplayer_peer as SteamMultiplayerPeer).get_steam_id_for_peer_id(peer_id)
+	var peer_info: PlayerInfo = Lobby.players.get(steam_id)
+	peer_info.display_name = Steam.getFriendPersonaName(steam_id)
+	peer_info.steam_id = steam_id
+
+
+func _on_peer_disconnected(peer_id: int) -> void:													# TODO REMOVE THIS IS JUST TO TEST WHAT TRIGGERS FIRST, multiplayer.peer_connected or this
+	if Lobby.players.has(peer_id):
+		Lobby.clear_player_info(peer_id)
+	else:
+		print_debug("in SteamMultiplayerLobby._on_peer_disconnected(%d), somehow dont have the peer_id in the players[] dict" % peer_id)
