c19fb4caf804eda4b9f09fb0bd54d6cac243d033
Theodor <fracter.giftogod@gmail.com>
2026-02-09 00:53:48 +0100

    Fix pause menu quit-to-main-menu button

--- src/ui/menus/pause_menu.gd
+++ src/ui/menus/pause_menu.gd
@@ -3,15 +3,15 @@ extends Panel
 @onready var button: Button = $Button
 @onready var label: Label = $Label
 
-const MAIN = preload("uid://bp3lhs80g85ky")
-
+const MAIN_SCENE_PATH = "res://src/Main.tscn"
 
 func _ready() -> void:
 	button.pressed.connect(_on_main_menu_pressed)
 
 
 func _on_main_menu_pressed() -> void:
-	get_tree().change_scene_to_packed(MAIN)
+	# NOTE DON'T SWAP SCENES HERE. let Lobby.leave_lobby() handle it. To avoid breaking Node.multiplayer property such before lobby quit.
+	Lobby.leave_lobby("Returning to main menu")
 
 func _input(event: InputEvent) -> void:
 	if event.is_action_pressed(&"ui_cancel"):
