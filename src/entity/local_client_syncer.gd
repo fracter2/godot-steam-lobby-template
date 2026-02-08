b9bb05a3f241c5b17f3cca14b0897f4f952dc79f
Theodor <fracter.giftogod@gmail.com>
2026-02-08 23:05:17 +0100

    Add local client syncer script for Player and DummyPlayer nodes

---
+++ src/entity/local_client_syncer.gd
@@ -0,0 +1,23 @@
+class_name LocalClientSyncronizer
+extends MultiplayerSynchronizer
+
+#@onready var player: Player = $".."
+@export var player: DummyPlayer
+
+@export var position: Vector2 = Vector2.ZERO
+
+
+func _enter_tree() -> void:
+	#set_multiplayer_authority(player.peer_id)
+	pass
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	if is_multiplayer_authority():
+		var input_dir: Vector2 = Vector2(Input.get_axis(&"move_left", &"move_right"),  Input.get_axis(&"move_up", &"move_down"))
+		position += input_dir * player.speed * delta
