08c3c133bef0427f96489947ff954c0cd7b75cc6
Theodor <fracter.giftogod@gmail.com>
2026-02-09 01:02:37 +0100

    Fix remote peers not setting player_info correctly by having peer_id setter also set player_info

--- src/entity/player_entity.gd
+++ src/entity/player_entity.gd
@@ -6,6 +6,7 @@ extends Node2D
 	set(id):
 		peer_id = id
 		local_client_syncronizer.set_multiplayer_authority(id)
+		player_info = Lobby.players.get(id)
 
 @export var speed: float = 100
 
@@ -21,9 +22,9 @@ var player_info: PlayerInfo = null:
 			player_info.display_name_set.disconnect(_update_name)
 			player_info.nickname_set.disconnect(_update_name)
 		player_info = info
-		if player_info != null:
-			player_info.display_name_set.connect(_update_name)
-			player_info.nickname_set.connect(_update_name)
+		if info != null:
+			info.display_name_set.connect(_update_name)
+			info.nickname_set.connect(_update_name)
 			_update_name()
 	#get():	# NOTE This should still be proovided by default
 	#	return player_info
@@ -32,7 +33,8 @@ var player_info: PlayerInfo = null:
 
 func _enter_tree() -> void:
 	if Lobby.players.has(peer_id):
-		player_info = Lobby.players.get(peer_id)
+		#player_info = Lobby.players.get(peer_id)
+		_update_name()
 	else:
 		push_error("PlayerEntity at %s \n -> peer_id %d set but player_info not found!" % [get_path(), peer_id])
 
@@ -50,6 +52,8 @@ func _physics_process(_delta: float) -> void:
 
 
 func _update_name() -> void:
+	if player_info == null: return
+
 	if not player_info.nickname.is_empty(): 		name_label.text = player_info.nickname
 	elif not player_info.display_name.is_empty():	name_label.text = player_info.display_name
 	else: 											name_label.text = "DefaultName"
