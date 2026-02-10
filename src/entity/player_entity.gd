75aa28f0573cd233adbde1eee02d66951855059b
Theodor <fracter.giftogod@gmail.com>
2026-02-11 00:19:07 +0100

    Reorganize player entity player_info setter

--- src/entity/player_entity.gd
+++ src/entity/player_entity.gd
@@ -18,14 +18,10 @@ extends Node2D
 
 var player_info: PlayerInfo = null:
 	set(info):
-		if player_info != null:
-			player_info.display_name_set.disconnect(_update_name)
-			player_info.nickname_set.disconnect(_update_name)
+		_disconnect_player_info(player_info)
 		player_info = info
-		if info != null:
-			info.display_name_set.connect(_update_name)
-			info.nickname_set.connect(_update_name)
-			_update_name()
+		_connect_player_info(info)
+
 	#get():	# NOTE This should still be proovided by default
 	#	return player_info
 
@@ -51,9 +47,28 @@ func _physics_process(_delta: float) -> void:
 	position = local_client_syncronizer.position
 
 
-func _update_name() -> void:
-	if player_info == null: return
 
+func _on_name_set(_new_name: String = "") -> void:
 	if not player_info.nickname.is_empty(): 		name_label.text = player_info.nickname
 	elif not player_info.display_name.is_empty():	name_label.text = player_info.display_name
 	else: 											name_label.text = "DefaultName"
+
+
+func _on_avatar_set(_new_avatar: Image = null) -> void:
+	if not player_info.avatar_small == null:
+		# TODO SET AVATAR PIC
+		pass
+func _disconnect_player_info(player: PlayerInfo) -> void:
+	if player != null:
+		player.display_name_set.disconnect(_on_name_set)
+		player.nickname_set.disconnect(_on_name_set)
+		player.avatar_small_set.disconnect(_on_avatar_set)
+
+
+func _connect_player_info(player: PlayerInfo) -> void:
+	if player != null:
+		player.display_name_set.connect(_on_name_set)
+		player.nickname_set.connect(_on_name_set)
+		player.avatar_small_set.connect(_on_avatar_set)
+		_on_name_set()
+		_on_avatar_set()
