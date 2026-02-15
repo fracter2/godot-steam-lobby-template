00b0995541f640d310306b9ee919088809a1bf76
Theodor <fracter.giftogod@gmail.com>
2026-02-15 02:00:07 +0100

    In game.gd rename signal callbacks and remove redundant prints

--- content/demo/game.gd
+++ content/demo/game.gd
@@ -15,7 +15,7 @@ var player_nodes: Dictionary[int, PlayerEntity] = {}
 #
 
 func _ready() -> void:
-	Lobby.lobby_entered.connect(_on_connected)										# NOTE Local lobby_entered only
+	Lobby.lobby_entered.connect(_on_lobby_entered)										# NOTE Local lobby_entered only
 	Lobby.lobby_exiting.connect(_on_lobby_exiting)	#, ConnectFlags.CONNECT_DEFERRED # NOTE Deffered to avouid quitting in the middle of processing... Theoretically helpfull
 
 	multiplayer.peer_connected.connect(_on_peer_connected)
@@ -27,7 +27,7 @@ func _ready() -> void:
 	player_spawner.despawned.connect(_check_if_player_despawned)
 
 	if Lobby.is_in_lobby():
-		_on_connected()
+		_on_lobby_entered()
 	else:
 		push_warning("Started game without being in a lobby! Is this ok?")
 		# NOTE Lobby.lobby_entered will emit after entering a lobby, with the same callback _on_connected()
@@ -43,8 +43,8 @@ func _exit_tree() -> void:
 # ---- SIGNALS ----
 #
 
-func _on_connected() -> void:
-	Log.pprint("Peer_%d: Lobby connected, setting game multiplayer authority" % multiplayer.get_unique_id())
+func _on_lobby_entered() -> void:
+	Log.pprint("GAME: JUST JOINED AS PEER %d" % [multiplayer.get_unique_id()])
 	multiplayer.multiplayer_peer = Lobby.multiplayer.multiplayer_peer
 	assert(multiplayer.multiplayer_peer != null, "obviously this shoulda not be null")
 	if multiplayer.is_server():
@@ -57,14 +57,14 @@ func _on_lobby_exiting(message: String) -> void:
 
 
 func _on_peer_connected(peer_id: int) -> void:
-	Log.pprint("Peer_%d: Peer connected with id: %d" % [multiplayer.get_unique_id(), peer_id])
 	if multiplayer.is_server():
 		_spawn_player(peer_id)
 
 
 func _on_peer_disconnected(peer_id: int) -> void:
+	Log.pprint("GAME: PEER %d DISCONNECTED" % peer_id)
 	if multiplayer == null: return 		# NOTE When host disconnectes and the scene changes through _on_lobby_exiting(), this callback still remains, and multiplayer is set to null.
-	Log.pprint("Peer_%d: calling _on_peer_disconnected() on peer_%d" % [multiplayer.get_unique_id(), peer_id])
+	#Log.pprint("Peer_%d: calling _on_peer_disconnected() on peer_%d" % [multiplayer.get_unique_id(), peer_id])
 	if multiplayer.is_server():
 		if not player_nodes.has(peer_id):
 			push_warning("Peer disconnected but was not added to player_info anyway...")
@@ -78,7 +78,7 @@ func _check_if_player_spawned(node: Node) -> void:
 	assert(not multiplayer.is_server(), "_on_entity_spawned() should only be called by non-servers, as described in the MultiplayerSpawner signal description.")
 	if node is PlayerEntity:
 		var peer_id: int = (node as PlayerEntity).peer_id
-		Log.pprint("Peer_%d: calling _check_if_player_spawned() on peer_%d" % [multiplayer.get_unique_id(), peer_id])
+
 		assert(not player_nodes.has(peer_id), "in _on_entity_spawned(), a new player node shouldn't already be registered here. obviously.")
 		player_nodes[peer_id] = node
 
@@ -98,7 +98,6 @@ func _check_if_player_despawned(node: Node) -> void:
 func _spawn_player(id: int) -> void:
 	assert(multiplayer.is_server())
 	assert(not player_nodes.has(id), "in _spawn_player() Spawning a player that is already registered!")
-	Log.pprint("Peer_%d: calling _spawn_player(%d)" % [multiplayer.get_unique_id(), id])
 
 	var player_instance: PlayerEntity = PLAYER_ENTITY_PRELOAD.instantiate()
 	player_instance.name = "player_peer_%d" % id
