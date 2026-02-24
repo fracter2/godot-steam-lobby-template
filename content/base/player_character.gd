cae38dbeced948c5c66d4c2c06f6d1f9bb2e96fa
Theodor <fracter.giftogod@gmail.com>
2026-02-25 00:40:44 +0100

    Add pointer to where player is looking

--- content/base/player_character.gd
+++ content/base/player_character.gd
@@ -8,16 +8,22 @@ extends Node2D
 @export_group("References")
 @export var player_entity: PlayerEntity
 @export var markers: Node2D
+@onready var direction_pointer: Node2D = $Direction
 
 var marker_preload: PackedScene = preload(PATHS.ENTITY_MARKER)
 
 
 func _physics_process(delta: float) -> void:
 	if is_multiplayer_authority():
+		# Movement
 		var input_dir: Vector2 = Vector2(Input.get_axis(&"move_left", &"move_right"),  Input.get_axis(&"move_up", &"move_down"))
 		position += input_dir * speed * delta
 
+		# Marker check
 		if Input.is_action_just_pressed(&"spawn_marker"):
 			var new_marker : Node2D = marker_preload.instantiate()
 			new_marker.position = get_global_mouse_position()
 			markers.add_child(new_marker, true)
+
+		# Direction arrow
+		direction_pointer.look_at(get_global_mouse_position())
