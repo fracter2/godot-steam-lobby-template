2087bbf6a9b66639121c49c9760d1e81c52a5a2b
Theodor <fracter.giftogod@gmail.com>
2026-06-27 21:59:34 +0200

    Separate branch logic into player_branch_manager.gd and rename Game to World

---
+++ content/demo/world.gd
@@ -0,0 +1,101 @@
+class_name World
+extends Node2D
+
+## This class is meant to be a general use world implementation that can be used as a basis for any kind of level.
+## This would be customized or replaced in an actual game.
+
+# TODO MAKE LIGHTER
+# TODO MAKE SO IT CAN LOAD LEVEL MAPS				# Or make separate node!
+# TODO MAKE SO IT CAN LOAD SAVE FILES				# Or make separate node!
+# TODO MAKE SO SERVER SENDS STATIC WORLD STATE		# Or make separate node!
+
+@export_group("References")
+@export var player_branch_manager: PlayerBranchManager
+@export var server_branch: Node2D
+@export var local_branch: Node2D
+
+const PLAYER_BRANCH = preload(PATHS.NETWORK_PLAYER_BRANCH)
+
+static var singleton: World = null
+
+
+#
+# ---- PROCEDURE ----
+#
+
+func _enter_tree() -> void:
+	assert(singleton == null)
+	singleton = self
+
+
+func _ready() -> void:
+	if Lobby.is_in_lobby():
+		_on_lobby_entered()
+	else:
+		push_warning("Started game without being in a lobby! Is this ok?")
+		Lobby.lobby_entered.connect(_on_lobby_entered, ConnectFlags.CONNECT_ONE_SHOT)				# Oneshot to clarify use-case
+
+	Lobby.lobby_exiting.connect(_on_lobby_exiting)	#, ConnectFlags.CONNECT_DEFERRED # NOTE Deffered to avouid quitting in the middle of processing... Theoretically helpfull
+	multiplayer.server_disconnected.connect(_on_lobby_exiting.bind("Server disconnected"))
+
+
+func _exit_tree() -> void:
+	# TODO NOTIFY SERVER BY DISCONNECTING FORMALLY (THROUGH LOBBY MAYBE?)
+	#multiplayer.disconnect() # NOTE NOT THIS, SINCE THE SAME PEER IS USED FOR LOBBY!
+	# NOTE Some errors may appear of "on_sync_recieve: Ignoring sync data from non-authority or for missing node". THIS IS OK! WE ARE QUITTING! LOL
+
+	singleton = null
+	pass
+
+
+#
+# ---- API ----
+#
+
+static func get_player_branch_manager() -> PlayerBranchManager:
+	return singleton.player_branch_manager
+
+
+## Returns the local users [PlayerBranch].
+static func get_user_player_branch() -> PlayerBranch:
+	return singleton.player_branch_manager.branches[singleton.multiplayer.get_unique_id()]
+
+
+## Returns the owning branch or null, of [param node]. IT just looks at the node hierarchy.
+static func get_player_branch_of(node: Node) -> PlayerBranch:
+	return singleton.player_branch_manager.get_player_branch_of_unchecked(node)
+
+
+## Adds the node to the tree under [property server_branch], of course with server authority set.
+static func spawn_server_owned(node: Node) -> void:
+	singleton.server_branch.add_child(node)
+
+
+## Adds the node to the tree under the local users [PlayerBranch], with it's own [MultiplayerSpawner].
+## This means They can have client multiplayer authority, like if [param node] has [constant GROUPS.PLAYER_OWNED] is set.
+## If called by host, still uses host branch.
+static func spawn_client_owned(node: Node) -> void:
+	var branch: PlayerBranch = singleton.player_branch_manager.branches[singleton.multiplayer.get_unique_id()]
+	branch.spawn_node(node)
+
+
+## Adds the node to the tree under [property local_entities]. Note that client-local (aka clientside or client-only) spawns don't
+## have a [MultiplayerSpawner] atached, but still has the multiplayer authority set to default (server, id 1).
+static func spawn_client_local(node: Node) -> void:
+	singleton.local_branch.add_child(node, true)
+
+
+#
+# ---- SIGNALS ----
+#
+
+func _on_lobby_entered() -> void:
+	Log.pprint("GAME: JUST JOINED AS PEER %d" % [multiplayer.get_unique_id()])
+	multiplayer.multiplayer_peer = Lobby.multiplayer.multiplayer_peer
+	assert(multiplayer.multiplayer_peer != null, "obviously this shoulda not be null")
+
+
+func _on_lobby_exiting(message: String) -> void:
+	Log.pprint("Quit level, message: %s" % message)
+	if is_inside_tree():
+		get_tree().change_scene_to_file(PATHS.MAIN_MENU)
---
+++ src/world_sync/player_branch_manager.gd
@@ -0,0 +1,122 @@
+class_name PlayerBranchManager
+extends Node2D
+
+
+const PLAYER_BRANCH = preload(PATHS.NETWORK_PLAYER_BRANCH)
+var branches: Dictionary[int, PlayerBranch] = {}
+var branch_spawner: MultiplayerSpawner											# NOTE Kept synced on remote peers by the MultiplayerSpawner
+
+
+#
+# ---- API ----
+#
+
+func get_player_branch_of_unchecked(node: Node) -> PlayerBranch:
+	var branch_name: StringName = get_path_to(node).get_name(0)
+	return get_node_or_null(NodePath(branch_name))
+
+
+#
+# ---- Procedure ----
+#
+
+func _enter_tree() -> void:
+	_add_multiplayer_spawner()
+	get_tree().node_added.connect(_check_player_ownership)
+
+
+func _ready() -> void:
+	if Lobby.is_in_lobby():
+		_connect_signals()
+	else:
+		Lobby.lobby_entered.connect(_connect_signals, ConnectFlags.CONNECT_ONE_SHOT)				# Oneshot to clarify use-case
+
+
+#
+# ---- Internal ----
+#
+
+func _add_multiplayer_spawner() -> void:
+	var spawner: MultiplayerSpawner = MultiplayerSpawner.new()
+	spawner.name = "PlayerBranchSpawner"
+	spawner.spawn_path = NodePath("..")
+	spawner.add_spawnable_scene(PATHS.NETWORK_PLAYER_BRANCH)
+	add_child(spawner, true)
+	branch_spawner = spawner
+
+
+func _connect_signals() -> void:
+	if multiplayer.is_server():
+		_spawn_player_branch(1)													# NOTE 1 is always the server peer_id
+		multiplayer.peer_connected.connect(_add_branch)
+		multiplayer.peer_disconnected.connect(_remove_branch)
+	else:
+		# NOTE MultiplayerSpawner soawned and despawned signals only emit on remote peers... so non-server clients
+		branch_spawner.spawned.connect(_add_branch_to_list)
+		branch_spawner.despawned.connect(_remove_branch_from_list)
+
+
+func _spawn_player_branch(id: int) -> void:
+	assert(multiplayer.is_server())
+	assert(not branches.has(id), "in _spawn_player_branch() Spawning a player that is already registered!")
+
+	var player_instance: PlayerBranch = PLAYER_BRANCH.instantiate()
+	player_instance.name = "player_peer_%d" % id
+	player_instance.peer_id = id
+	branches[id] = player_instance										# NOTE player_nodes is kept synced on remote peers by the MultiplayerSpawner signal callbacks
+	add_child(player_instance)
+
+
+func _check_player_ownership(node: Node) -> void:
+	var is_player_owned: bool = false
+	var recursive: bool = false
+
+	if node.is_in_group(GROUPS.PLAYER_OWNED_RECURSIVE):
+		is_player_owned = true
+		recursive = true
+	elif node.is_in_group(GROUPS.PLAYER_OWNED):
+		is_player_owned = true
+
+	if is_player_owned and is_ancestor_of(node):
+		node.set_multiplayer_authority(get_player_branch_of_unchecked(node).peer_id, recursive)
+
+
+#
+# ---- SERVER ONLY ----
+#
+
+func _add_branch(peer_id: int) -> void:
+	assert(multiplayer.is_server())
+	_spawn_player_branch(peer_id)
+
+
+func _remove_branch(peer_id: int) -> void:
+	#Log.pprint("GAME: PEER %d DISCONNECTED" % peer_id)
+	if multiplayer == null:
+		push_warning("attempt to remove branch when multiplayer == null! Should disconnect signal here!!")	# TODO Replace with signal disconnect where it makes sense, or keep without warning
+		return
+
+	assert(multiplayer.is_server())
+	assert(branches.has(peer_id), "Peer disconnected but was not added to player_info anyway...")
+	branches[peer_id].queue_free()
+	branches.erase(peer_id)
+
+
+#
+# ---- CLIENTS ONLY ----
+#
+
+func _add_branch_to_list(node: Node) -> void:
+	assert(not multiplayer.is_server(), "_on_entity_spawned() should only be called by non-servers, as described in the MultiplayerSpawner signal description.")
+	if node is PlayerBranch:
+		var peer_id: int = (node as PlayerBranch).peer_id
+		assert(not branches.has(peer_id), "in _on_entity_spawned(), duplicate player branch spawned!")
+		branches[peer_id] = node
+
+
+func _remove_branch_from_list(node: Node) -> void:
+	assert(not multiplayer.is_server(), "_on_entity_despawned() should only be called by non-servers, as described in the MultiplayerSpawner signal description.")
+	if node is PlayerBranch:
+		var peer_id: int = (node as PlayerBranch).peer_id
+		assert(branches.has(peer_id), "in _on_entity_despawned(), the deleted player should still be in the player_nodes dict!")
+		branches.erase(peer_id)
