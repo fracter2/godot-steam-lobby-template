249cdfc50d1a2358494f55a6b90236b963acc7dd
Theodor <fracter.giftogod@gmail.com>
2026-06-28 12:46:02 +0200

    make player branches spawn manually without MultiplayerSpawner, fixing delay and simplifying logic

--- src/world_sync/player_branch_manager.gd
+++ src/world_sync/player_branch_manager.gd
@@ -21,41 +21,17 @@ func get_player_branch_of_unchecked(node: Node) -> PlayerBranch:
 #
 
 func _enter_tree() -> void:
-	_add_multiplayer_spawner()
 	get_tree().node_added.connect(_check_player_ownership)
-	if Lobby.is_in_lobby():
-		_connect_signals()
-	else:
-		Lobby.lobby_entered.connect(_connect_signals, ConnectFlags.CONNECT_ONE_SHOT)				# Oneshot to clarify use-case
-
-
-func _ready() -> void:
-	pass
+	multiplayer.peer_connected.connect(_create_branch)
+	multiplayer.peer_disconnected.connect(_remove_branch)
+	for id: int in multiplayer.get_peers():
+		_create_branch(id)
+	_create_branch(multiplayer.get_unique_id())
 
 
 #
 # ---- Internal ----
 #
-
-func _add_multiplayer_spawner() -> void:
-	var spawner: MultiplayerSpawner = MultiplayerSpawner.new()
-	spawner.name = "PlayerBranchSpawner"
-	spawner.spawn_path = NodePath("..")
-	spawner.add_spawnable_scene(PATHS.NETWORK_PLAYER_BRANCH)
-	add_child(spawner, true)
-	branch_spawner = spawner
-
-
-func _connect_signals() -> void:
-	if multiplayer.is_server():
-		_create_branch(1)													# NOTE 1 is always the server peer_id
-		multiplayer.peer_connected.connect(_create_branch)
-		multiplayer.peer_disconnected.connect(_remove_branch)
-	else:
-		# NOTE MultiplayerSpawner soawned and despawned signals only emit on remote peers... so non-server clients
-		branch_spawner.spawned.connect(_add_branch_to_list)
-		branch_spawner.despawned.connect(_remove_branch_from_list)
-
 
 func _check_player_ownership(node: Node) -> void:
 	var is_player_owned: bool = false
@@ -71,19 +47,16 @@ func _check_player_ownership(node: Node) -> void:
 		node.set_multiplayer_authority(get_player_branch_of_unchecked(node).peer_id, recursive)
 
 
-#
-# ---- SERVER ONLY ----
-#
-
 func _create_branch(id: int) -> void:
-	assert(multiplayer.is_server())
 	assert(not branches.has(id), "in _spawn_player_branch() Spawning a player that is already registered!")
 
-	var player_instance: PlayerBranch = PLAYER_BRANCH.instantiate()
-	player_instance.name = "player_peer_%d" % id
-	player_instance.peer_id = id
-	branches[id] = player_instance										# NOTE player_nodes is kept synced on remote peers by the MultiplayerSpawner signal callbacks
-	add_child(player_instance)
+	var branch_instance: PlayerBranch = PLAYER_BRANCH.instantiate()
+	branch_instance.name = "player_peer_%d" % id
+	branch_instance.peer_id = id
+	branches[id] = branch_instance
+	add_child(branch_instance)
+	branches.sort()
+	move_child(branch_instance, branches.keys().bsearch(branch_instance.peer_id))
 
 
 func _remove_branch(peer_id: int) -> void:
@@ -96,23 +69,3 @@ func _remove_branch(peer_id: int) -> void:
 	assert(branches.has(peer_id), "Peer disconnected but was not added to player_info anyway...")
 	branches[peer_id].queue_free()
 	branches.erase(peer_id)
-
-
-#
-# ---- CLIENTS ONLY ----
-#
-
-func _add_branch_to_list(node: Node) -> void:
-	assert(not multiplayer.is_server(), "_on_entity_spawned() should only be called by non-servers, as described in the MultiplayerSpawner signal description.")
-	if node is PlayerBranch:
-		var peer_id: int = (node as PlayerBranch).peer_id
-		assert(not branches.has(peer_id), "in _on_entity_spawned(), duplicate player branch spawned!")
-		branches[peer_id] = node
-
-
-func _remove_branch_from_list(node: Node) -> void:
-	assert(not multiplayer.is_server(), "_on_entity_despawned() should only be called by non-servers, as described in the MultiplayerSpawner signal description.")
-	if node is PlayerBranch:
-		var peer_id: int = (node as PlayerBranch).peer_id
-		assert(branches.has(peer_id), "in _on_entity_despawned(), the deleted player should still be in the player_nodes dict!")
-		branches.erase(peer_id)
