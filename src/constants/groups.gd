46e3cbac1092afdcc0caa11bd4780720ab16bc1d
Theodor <fracter.giftogod@gmail.com>
2026-07-02 22:15:23 +0200

    Update group names for player authority setting, and add option to set server authority

--- src/constants/groups.gd
+++ src/constants/groups.gd
@@ -6,5 +6,7 @@ extends Object
 
 # Dis scrip is audogenewaded by auto_update tool button
 
-const PLAYER_OWNED: StringName = &"Player Owned"
-const PLAYER_OWNED_RECURSIVE: StringName = &"Player Owned Recursive"
+const SET_PLAYER_AUTHORITY_NO_CHILDREN: StringName = &"Set Player Authority No Children"
+const SET_SERVER_AUTHORITY_NO_CHILDREN: StringName = &"Set Server Authority No Children"
+const SET_PLAYER_AUTHORITY: StringName = &"Set Player Authority"
+const SET_SERVER_AUTHORITY: StringName = &"Set Server Authority"
