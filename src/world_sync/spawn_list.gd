601fec4d8f06f429f62a6cd38a1d5929ed47ae31
Theodor <fracter.giftogod@gmail.com>
2026-07-04 18:00:54 +0200

    Add interface funcs to Spawnlist to add or validate paths

--- src/world_sync/spawn_list.gd
+++ src/world_sync/spawn_list.gd
@@ -11,7 +11,7 @@ const valid_filetypes_combined: String = "*.tscn,*.res,*.scn,*.dae,*.obj,*.escn,
 
 ## Parses [member list] for invalid file paths
 @warning_ignore("unused_private_class_variable")
-@export_tool_button("Check Paths") var _check_paths_tool: Callable = _check_invalid_paths
+@export_tool_button("Check Paths") var _check_paths_tool: Callable = _re_check_paths
 
 @export_storage var uid_paths: Dictionary[int, String]
 @export_storage var raw_paths: PackedStringArray
@@ -31,31 +31,59 @@ func has_path(path: String) -> bool:
 		return raw_paths.has(path) or uid_paths.values().has(path)
 
 
+func add_path(path: String) -> void:
+	if _is_valid_path(path):
+		var uid: int = ResourceUID.text_to_id(path)
+		if ResourceUID.has_id(uid):
+			uid_paths.set(uid, ResourceUID.get_id_path(uid))
+		else:
+			raw_paths.push_back(path)
+	else:
+		invalid_paths.push_back(path)
+
+
+## Gets all valid paths combined in one [PackedStringArray], preffering UID path strings when available.
+func get_valid_paths() -> PackedStringArray:
+	var r: PackedStringArray = []
+	for i: int in uid_paths.keys():
+		r.push_back(ResourceUID.id_to_text(i))
+	r.append_array(raw_paths)
+	return r
+
+
+## Get all paths combined in one [PackedStringArray], INCLUDING INVALID PATHS. UID paths are kept as such, to save space and because it's cheaper to convert UID to a path.
+func get_valid_and_invalid_paths() -> PackedStringArray:
+	var r: PackedStringArray = get_valid_paths()
+	r.append_array(invalid_paths)
+	return r
+
+
 #
 # ---- Editor Functionality ----
 #
 
-func _check_invalid_paths() -> void:											# TODO REMAKE WITH UPDATED LOGIC, MAKE REFRESH SETT BY COMIBINING AND SEPPARATING AGAIN
-	#invalid_path_indexes = _get_invalid_path_indexes(list)
-	notify_property_list_changed()	# To reveal / hide the cleanup tool button
-	#if invalid_path_indexes.is_empty():
-	#	print("Check Paths: All Good!")
-	#else:
-	#	printerr("Check Paths: %d Invalid" % invalid_path_indexes.size())
+func _re_check_paths() -> void:
+	var combined_paths: PackedStringArray = get_valid_and_invalid_paths()
+	_parse_new_list(combined_paths)
 
 
-func _remove_invalid() -> void:
-	print("Removing invalid indexes...")										# TODO NOTIFY THROUGH TOAST
+func _remove_invalid() -> void:													# TODO TEST IF THIS CRASHES IN BUILD, since EditorInterface doesn't exists...
+	if invalid_paths.is_empty():
+		EditorInterface.get_editor_toaster().push_toast("No invalid spawn paths!")
+		return
+
 	if Engine.is_editor_hint():
 		var undo_redo: EditorUndoRedoManager = EditorInterface.get_editor_undo_redo()
 		undo_redo.create_action("Remove invalid paths from Spawnlist")
 		undo_redo.add_do_property(self, propertyname_invalid_paths, [])
 		undo_redo.add_undo_property(self, propertyname_invalid_paths, invalid_paths)
 		undo_redo.commit_action()
+		notify_property_list_changed()
+		print("Removed invalid paths!")
+		EditorInterface.get_editor_toaster().push_toast("Removed invalid paths!")
 	else:
 		invalid_paths.clear()
 
-	notify_property_list_changed()
 
 
 func _get_property_list() ->  Array[Dictionary]:
@@ -99,7 +127,7 @@ func _get(property: StringName) -> Variant:
 		return p
 
 	elif property == &"SCENE_PATHS":
-		var p: PackedStringArray = _get_all_paths()
+		var p: PackedStringArray = get_valid_and_invalid_paths()
 		return p
 
 	else:
@@ -125,14 +153,7 @@ func _parse_new_list(paths: PackedStringArray) -> void:
 	invalid_paths.clear()
 
 	for s: String in paths:
-		if _is_valid_path(s):
-			var uid: int = ResourceUID.text_to_id(s)
-			if ResourceUID.has_id(uid):
-				uid_paths.set(uid, ResourceUID.get_id_path(uid))
-			else:
-				raw_paths.push_back(s)
-		else:
-			invalid_paths.push_back(s)
+		add_path(s)
 
 	notify_property_list_changed()	# To reveal / hide the cleanup tool button
 	if invalid_paths.is_empty():
@@ -141,15 +162,6 @@ func _parse_new_list(paths: PackedStringArray) -> void:
 	else:
 		printerr("Check Paths: %d Invalid" % invalid_paths.size())
 		EditorInterface.get_editor_toaster().push_toast("Check Paths: %d Invalid" % invalid_paths.size(), EditorToaster.SEVERITY_ERROR, "You prob input an empty path or misspelled one")
-
-
-func _get_all_paths() -> PackedStringArray:
-	var r: PackedStringArray = []
-	for i: int in uid_paths.keys():
-		r.push_back(ResourceUID.id_to_text(i))
-	r.append_array(raw_paths)
-	r.append_array(invalid_paths)
-	return r
 
 
 func _get_invalid_path_indexes(paths: PackedStringArray) -> PackedInt32Array:
